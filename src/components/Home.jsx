2920e76e066e47829adafc7629e37ae042c00d5c
Guilherme S <guinho.shuto@gmail.com>
2022-06-28 22:42:58 -0400

    recebe feras online

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect }from 'react';
+import axios from 'axios';
 import Adsense from './Adsense';
 
 const Home = () => {
+    const [feras, setFeras] = useState([]) 
+
+    async function getFerasOnline(){
+        const ferasOnline = await axios.get("https://feras-leaderboards.herokuapp.com/team")
+        setFeras(ferasOnline.data)
+    } 
+
+    useEffect(() => {
+        getFerasOnline()
+    }, [])
+
     return(
         <div className="team">
+            {console.log(feras)}
             <nav className="team-header">
                 <div className="twitch-icon"></div>
                 <ul>
